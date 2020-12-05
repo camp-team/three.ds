16a76bfb7853b13234629d5cfd2a03e85b83343e
shhymmt <mail@shhymmt.com>
2020-12-05 21:23:13 +0900

    welcome画面マークアップ - 3H #15

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
-import {MatButtonModule} from '@angular/material/button';
-
 
 @NgModule({
   declarations: [WelcomeComponent],
   imports: [
     CommonModule,
     WelcomeRoutingModule,
-    MatButtonModule
-  ]
+    MatButtonModule,
+    MatRippleModule,
+  ],
 })
-export class WelcomeModule { }
+export class WelcomeModule {}
