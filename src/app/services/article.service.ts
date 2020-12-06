37ccbf73bb064901e6834f4791268a41d37b9cec
hide-a1 <hide210g@gmail.com>
2020-12-06 10:24:15 +0900

    機能：3Dファイルを取得できる形で追加実装 - 4H Fixes #3

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { Router } from '@angular/router';
 import { Article } from '../interfaces/Article';
 
@@ -10,13 +11,19 @@ export class ArticleService {
 
   constructor(
     private db: AngularFirestore,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) { }
 
   async createArticle(
     article: Omit<Article, 'id'>
   ): Promise<void> {
     const id = this.db.createId();
+    console.log(id);
+    if (article.image !== undefined) {
+      console.log(article.image);
+      article.image = await this.setImageToStorage(id, article.image);
+    }
     const newValue: Article = {
       id,
       ...article,
@@ -24,5 +31,12 @@ export class ArticleService {
     await this.db.doc<Article>(`posts/${id}`).set(newValue).then(() => {
       this.router.navigateByUrl(`article/${id}`);
     });
+  }
+
+  async setImageToStorage(articleId: string, file: File): Promise<string> {
+    const result = await this.storage
+      .ref(`posts/${articleId}`)
+      .put(file);
+    return result.ref.getDownloadURL();
   }
 }
