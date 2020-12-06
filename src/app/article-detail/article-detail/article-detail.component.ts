6c159e459714b42b1810315d201dc7da3147e88f
shhymmt <mail@shhymmt.com>
2020-12-06 15:32:10 +0900

    機能：記事編集機能実装 - 4H Fixes #12

--- src/app/article-detail/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail/article-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Article } from 'src/app/interfaces/Article';
 import { ArticleService } from 'src/app/services/article.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-article-detail',
@@ -16,12 +18,42 @@ export class ArticleDetailComponent implements OnInit {
   article$: Observable<Article> = this.articleService.getArticle(
     this.articleId
   );
+  titleMaxLength = 40;
+  descriptionMaxLength = 150;
+  form = this.fb.group({
+    title: [
+      '',
+      [Validators.required, Validators.maxLength(this.titleMaxLength)],
+    ],
+    description: ['', [Validators.maxLength(this.descriptionMaxLength)]],
+  });
+
+  // user$: Observable<UserData> = this.userService.getUser();
 
   constructor(
     private articleService: ArticleService,
     private route: ActivatedRoute,
-    public authServise: AuthService
+    public authServise: AuthService,
+    private userService: UserService,
+    private fb: FormBuilder
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.article$.subscribe((article) => {
+      this.form.patchValue(article);
+    });
+    this.form.markAsPristine();
+  }
+
+  update(): void {
+    const formData = this.form.value;
+    const newValue: Omit<
+      Article,
+      'id' | 'createdAt' | 'ownerId' | 'image' | 'updatedAt' | 'likeCount'
+    > = {
+      title: formData.title,
+      description: formData.description,
+    };
+    this.articleService.updateArticle(this.articleId, newValue);
+  }
 }
