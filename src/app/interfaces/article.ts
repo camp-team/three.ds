0acc715e1bbdc0a1a3ebc31588d676c3909b511d
kokiinoue45 <koki_inoue45@icloud.com>
2020-12-07 18:41:19 +0900

    fix

--- src/app/interfaces/article.ts
+++ src/app/interfaces/article.ts
@@ -1,13 +1,13 @@
+import { Timestamp } from '@google-cloud/firestore';
 import { UserData } from './user-data';
-
 export interface Article {
   id: string;
   ownerId: string;
   title: string;
   description?: string;
   image: string | any;
-  createdAt: firebase.default.firestore.Timestamp;
-  updatedAt: firebase.default.firestore.Timestamp;
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
   likeCount?: number;
 }
 
